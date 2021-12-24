420dcac1cc80fbbed258938365997a458581ea98
datoxx <davit.ardzenadze.05@gmail.com>
2021-12-24 22:01:55 +0400

    2.2

---
+++ part2/courseinfo-update/src/App.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development ',
+    parts: [
+      {
+        name: 'Fundamentals of React ',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data ',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component ',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux ',
+        exercises: 11,
+        id: 4
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course =({course}) => {
+  return (
+    <div>
+    <Header course={course} />
+    <Content course={course} />
+    <Total course={course} />
+  </div>
+  );
+}
+
+const Header = ({course}) => {
+  return(
+    <h1>{course.name}</h1>
+  );
+}
+
+const Content = ({course}) => {
+  return(
+    <div>
+      <Part part={course.parts[0]} />
+      <Part part={course.parts[1]} />
+      <Part part={course.parts[2]} />
+      <Part part={course.parts[3]} />
+    </div>
+  );
+}
+
+const Part = ({part}) => {
+  return(
+    <p>{part.name}{part.exercises}</p>
+  );
+}
+
+const Total = ({course}) => {
+  return (
+    <p> total of exercises
+      {course.parts[0].exercises +
+        course.parts[1].exercises +
+        course.parts[2].exercises +
+        course.parts[3].exercises}
+    </p>
+  );
+}
+
+
+
+export default App;
+
