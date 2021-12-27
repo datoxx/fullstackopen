711ca37dea87612a6f316b37a75200e35ae5b5ce
datoxx <davit.ardzenadze.05@gmail.com>
2021-12-27 23:42:40 +0400

    2.12 with comment

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,42 +3,44 @@ import axios from 'axios'
 
 
 function App() {
+  // array store the countries objects
+  const [countries, setCountries] = useState([])
+  // filter input  value
+  const [filter, setFilter] = useState('');
+  //const [displayCountries, setDisplayCountries] = useState('to many matches, specify filter')
 
-const [countries, setCountries] = useState([])
-const [filter, setFilter] = useState('');
-//const [displayCountries, setDisplayCountries] = useState('to many matches, specify filter')
+  // fetch data from this link https://restcountries.com/v3.1/all
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+      console.log(response.data);
+      setCountries(response.data)
+      })
+  }, [])
 
-useEffect(() => {
+  // filter input's event handle
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value.toLowerCase())
+    
+  }
 
-  axios
-    .get('https://restcountries.com/v3.1/all')
-    .then(response => {
-     console.log(response.data);
-     setCountries(response.data)
-    })
-}, [])
-
-
-const handleFilterChange = (e) => {
-  setFilter(e.target.value.toLowerCase())
-  
-}
-
-const filterCountries = filter === '' ? countries
-  : countries.filter(list => list.name.common.toLowerCase().includes(filter))
+  // user filter countries  by input
+  const filterCountries = filter === '' ? countries
+    : countries.filter(list => list.name.common.toLowerCase().includes(filter))
 
 
-  const display = filterCountries.length > 10 
-  ? 'type for search your countries'
-  : filterCountries.length > 1 && filterCountries.length < 10 ? 
-      (filterCountries.map(item => {
-        return <p key={item.name.common}>{item.name.common}</p>
-      }))                               
-  : (filterCountries.map(item => {
+  const display = filterCountries.length > 10  // << if true 
+  ? 'type for search your countries' // this happen
+  : filterCountries.length > 1 && filterCountries.length < 10  //  << else if true
+  ? (filterCountries.map(item => {             // this happen  
+    <p key={item.name.common}>{item.name.common}</p>
+    }))                           
+  : (filterCountries.map(item => {   // else this happen
     console.log(item);
     return (
-      <div key={item.name.common}>
-        <h1>{item.name.common}</h1>
+      <div key={item.name.common}> 
+        <h1>{item.name.common}</h1> 
         <p>continents: {item.continents}</p>
         <p>capital: {item.capital}</p>
         <p>population: {item.population}</p>
@@ -46,9 +48,9 @@ const filterCountries = filter === '' ? countries
         <img src={item.flags.png} alt="photo" />
       </div>
       )
-  }))     
-
-
+  }))   
+  
+  
   return (
     <div>
       find countries: <input 
