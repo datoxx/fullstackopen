f434ca5973e7e39db598a5ea5ec86558c55b5f01
datoxx <davit.ardzenadze.05@gmail.com>
2021-12-28 16:19:10 +0400

    2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -10,6 +10,7 @@ function App() {
   // filter input  value
   const [filter, setFilter] = useState('');
 
+  
   // fetch data from this link https://restcountries.com/v3.1/all
   useEffect(() => {
     axios
@@ -25,16 +26,32 @@ function App() {
     
   }
 
+  // set country name in input filter when button is clicked
+  const handleButton = (e) => {
+    setFilter(e.target.value.toLowerCase())
+  }
+
   // user filter countries  by input
-  const filterCountries = filter === '' ? countries
+  const filterCountries = filter === '' 
+    ? countries
     : countries.filter(list => list.name.common.toLowerCase().includes(filter))
-  
+
+  // if countries list is more then 10 display text 'type for search your countries',
+  // if countries list is less then 10 show all with button, and when button is clicked display country info
+  // if user enter full name of countries,  show info about this country
   const show = filterCountries.length > 10 
     ? 'type for search your countries'
     : filterCountries.length > 1 && filterCountries.length < 10 
-    ? filterCountries.map(item => <p key={item.population}>{item.name.common}</p>)                           
+    ? filterCountries.map(item => {
+        return (
+          <div key={item.population}>
+            {item.name.common}  <button value={item.name.common} onClick={handleButton}>show</button>
+          </div>
+        );
+      })                           
     : filterCountries.map(item => <Country key={item.name.common} item={item} />)
     
+
   return (
     <div>
      <Filter filter={filter} handleFilterChange={handleFilterChange} />
