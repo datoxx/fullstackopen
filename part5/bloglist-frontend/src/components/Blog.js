9c5fdcb088d732c3d8cf75687fa78dbe6184e76f
datoxx <davit.ardzenadze.05@gmail.com>
2022-02-20 21:43:21 +0400

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-     <p> title: {blog.title}</p>
-     <p> author: {blog.author}</p> 
+import React, { useState } from 'react';
+
+
+const Blog = ({blog}) => {
+  const [details, setDetails] = useState(false);
+  const [buttonLabel, setButtonLabel] = useState('view');
+  const showDetails = { display: details ? '' : 'none' };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const likeBlog = () => {
+
+  }
+
+  const toggleDetails = () => {
+    setDetails(!details)
+    setButtonLabel(details ? "view" : "hide" )
+  }
+
+return(
+  <div style={blogStyle}>
+     <p> {blog.title} by {blog.author}</p>
+     <button onClick={toggleDetails}>{buttonLabel}</button>
+     <div style={showDetails}>
+        <a href={blog.url}>{blog.url}</a>
+        <p>
+          likes
+          <button onClick={likeBlog}>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+     </div>
   </div>  
 )
+  
+}
 
 export default Blog
