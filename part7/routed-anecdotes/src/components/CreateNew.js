261b4b2869f6cc92327517b16c8457d5bdc9e145
datoxx <davit.ardzenadze.05@gmail.com>
2022-03-07 15:18:44 +0400

    7.4 add useFiled custom hook for simplify the anecdote creation form

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,24 +1,24 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
 
 const CreateNew = ({addNew, setNotification}) => {
     const navigate = useNavigate()
 
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+    const content = useField("text")
+    const author = useField("text")
+    const info = useField("text")
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
 
-    setNotification(`a new anecdote - ${content}`)
+    setNotification(`a new anecdote - ${content.value}`)
     setTimeout(() =>  setNotification(''), 5000)
 
     navigate('/')
@@ -30,15 +30,15 @@ const CreateNew = ({addNew, setNotification}) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} required />
+          <input {...content} required />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} required />
+          <input {...author} required />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} required />
+          <input {...info} required />
         </div>
         <button>create</button>
       </form>
