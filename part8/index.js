b28b89cffa6f6a28057a0c4d76808de3434a58d9
datoxx <davit.ardzenadze.05@gmail.com>
2022-04-05 19:55:36 +0400

    8.3, 8.4, 8.5, 8.6, 8.7

--- part8/index.js
+++ part8/index.js
@@ -27,15 +27,6 @@ let authors = [
   },
 ]
 
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
 
 let books = [
   {
@@ -88,7 +79,6 @@ let books = [
     genres: ['classic', 'revolution']
   },
 ]
-
 
 const typeDefs = gql`
  type Author {
