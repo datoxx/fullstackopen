7fe1c9b01d55b44e8a55c85b062ed8f39432b2f6
datoxx <davit.ardzenadze.05@gmail.com>
2022-04-15 22:26:28 +0400

    add core

--- part9/patientor/backend/index.ts
+++ part9/patientor/backend/index.ts
@@ -1,9 +1,11 @@
 const express = require('express')
+import cors from 'cors';
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get('/api/ping', (_req: any, res: { send: (arg: string) => void }) => {
   console.log('someone pinged here');
